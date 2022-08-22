3e50e212017c45cf1adc43652c25c4331b25ce8a
amegon <amegon@gmail.com>
2022-08-22 18:52:39 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # zabbix_partition_utility
+
+## LICENSE
+This software is released under the MIT License, see LICENSE.txt.
+## 概要
+MariaDB で作成された Zabbix DB に RANGE パーティションを作成する SQLコマンドを書き出す補助スクリプトです。
+
+## 仕様
+本スクリプトは MariaDB で稼働している Zabbix の DB を1か月単位の RANGE でパーティショニングするための SQLコマンドを作成、表示します。
+SQL コマンドは以下の仕様に沿って作成されます。
+
+- MariaDB で稼働している Zabbix の DB を1か月単位の RANGE でパーティショニングします。
+- スクリプト実行日時の月を初月として12か月分のパーティションを作成します。
+- パーティションは以下テーブルに対して作成します。
+    - history
+    - history_uint
+    - history_str
+    - history_log
+    - history_text
+    - trends
+    - trends_uint
+### メモ
+- 現時点より過去の月のパーティションを作成したい場合にや12か月分以上のパーティションを作成したい場合には、スクリプトの DEFINITION にある変数を編集して実行してください。
+
+## 注意事項
+- 本スクリプトで作成される SQL コマンドで作成されるパーティションは MAXVALUE は設定していません。適宜新しいパーティションを作成して運用してください。
+- 本スクリプトで作成される SQL コマンドは初回パーティショニング作成用です。冪等性はありません。
+## 使用方法
+- パーティショニングを実施したいサーバーに本リポジトリを展開
+- 展開したリポジトリ配下に移動
+- 以下のコマンドを実行して SQLコマンドが記載されたファイルを作成
+
+```
+sh make_partition_mariadb_sql.sh > create.sql
+```
+
+- 以下のコマンドで sql を実行
+
+```
+mysql -u <db_username> -p zabbix < create.sql
+```
+
+## 参考
+### パーティションの状態確認
+DB にログインして以下のコマンドを実行
+
+```
+SELECT TABLE_NAME,PARTITION_NAME,DATA_FREE,TABLE_ROWS FROM INFORMATION_SCHEMA.PARTITIONS WHERE TABLE_SCHEMA = 'zabbix' AND TABLE_NAME = '<テーブル名>';
+```
---
+++ make_partition_mariadb_sql.sh
@@ -0,0 +1,130 @@
+    #!/bin/bash
+
+    # 動作仕様
+    # 以下のようなSQL文をテーブル毎 (history, history_uint, history_str, history_text, history_log, trends, trends_uint) に作成する
+    #
+    # ALTER TABLE `history` PARTITION BY RANGE (clock) (
+    # PARTITION p2021_06 VALUES LESS THAN (UNIX_TIMESTAMP("2021-07-01 00:00:00")) ENGINE = InnoDB,
+    # PARTITION p2023_08 VALUES LESS THAN (UNIX_TIMESTAMP("2023-09-01 00:00:00")) ENGINE = InnoDB
+    # );
+
+    ### DEFINITION
+    # 現在時から過去何か月分さかのぼったパーティションを作成するか指定
+    GLOBAL_PAST_PARTITION_COUNT=0
+    # 全部で何か月分のパーティションを作成するか指定
+    GLOBAL_PARTITION_COUNT=12
+
+    ### 関数
+
+    # set_primarykey_sql
+    #
+    # テーブルのプライマリキー設定を変更するコマンドを記載
+    # 引数
+    #    なし
+    #
+    function set_primarykey_sql () {
+        # history
+        echo "DROP INDEX \`history_1\` ON \`history\`;"
+        echo "ALTER TABLE \`history\` ADD PRIMARY KEY (\`itemid\`, \`clock\`, \`ns\`);"
+        # history_uint
+        echo "DROP INDEX \`history_uint_1\` ON \`history_uint\`;"
+        echo "ALTER TABLE \`history_uint\` ADD PRIMARY KEY (\`itemid\`, \`clock\`, \`ns\`);"
+        # history_str
+        echo "ALTER TABLE \`history_str\` ADD PRIMARY KEY (\`itemid\`, \`clock\`, \`ns\`);"
+        # history_text
+        echo "ALTER TABLE \`history_text\` ADD PRIMARY KEY (\`itemid\`, \`clock\`, \`ns\`);"
+        # history_log
+        echo "ALTER TABLE \`history_log\` ADD PRIMARY KEY (\`itemid\`, \`clock\`, \`ns\`);"
+    }
+
+    # make_partition_sql_by_table
+    #
+    # テーブル用のパーティション作成コマンドを表示する
+    # 引数
+    #    $1 テーブル名
+    #    $2 パーティションの作成開始年
+    #    $3 パーティションの作成開始月
+    #    $4 作成するパーティションの数
+    #
+    function make_partition_sql_by_table () {
+        # 引数をセット
+        TABLE_NAME=$1
+        START_PARTITION_YEAR=$2
+        START_PARTITION_MONTH=$3
+        PARTITION_COUNT=$4
+
+        # 作成するパーティション数 = ループ回数としてセット
+        ROOP_COUNT=$PARTITION_COUNT
+
+        echo "ALTER TABLE \`${TABLE_NAME}\` PARTITION BY RANGE (clock) ("
+
+        PARTITION_YEAR=$START_PARTITION_YEAR
+        PARTITION_MONTH=$START_PARTITION_MONTH
+
+        for i in `seq 1 $ROOP_COUNT`
+        do
+            # ループの初回以外は必要に応じてパーティション月のカウントアップを行う。
+            if [[ $i -ne 1 ]]; then
+                PARTITION_MONTH=$(expr $PARTITION_MONTH + 1)
+
+                # パーティション作成月が13になった場合、パーティション作成年を +1 して作成付きは 1 にする 。
+                if [[ $PARTITION_MONTH -eq 13 ]]; then
+                    PARTITION_MONTH=1
+                    PARTITION_YEAR=$(expr $PARTITION_YEAR + 1)
+                fi
+                if [[ ${PARTITION_MONTH} =~ ^[0-9]$ ]]; then
+                    PARTITION_MONTH="0${PARTITION_MONTH}"
+                fi
+            fi
+            PARTITION_NAME="p${PARTITION_YEAR}${PARTITION_MONTH}"
+
+            # 行末にカンマをつけるかどうか確認。
+            # つける必要がある場合には END_CUNMA にカンマをセット。
+            END_CUNMA=""
+            if [[ $i -ne $ROOP_COUNT ]]; then
+                END_CUNMA=","
+            fi
+
+            echo "PARTITION ${PARTITION_NAME} VALUES LESS THAN (UNIX_TIMESTAMP(\"${PARTITION_YEAR}-${PARTITION_MONTH}-01 00:00:00\")) ENGINE = InnoDB${END_CUNMA}"
+
+        done
+
+        # 最終行に記載
+        echo ");"
+    }
+
+    ### Main
+
+    # 現在日時から日付情報を取得
+    CURRENT_YEAR=`date '+%Y'`
+    CURRENT_MONTH=`date '+%m'`
+
+    START_PARTITION_YEAR=$CURRENT_YEAR
+    START_PARTITION_MONTH=$CURRENT_MONTH
+
+    # 過去のパーティション作成用の調整
+    PAST_PARTITION_COUNT=$GLOBAL_PAST_PARTITION_COUNT
+
+    if [[ $PAST_PARTITION_COUNT -gt 0 ]]; then
+        for i in `seq 1 $PAST_PARTITION_COUNT`
+        do
+            START_PARTITION_MONTH=$(expr $START_PARTITION_MONTH - 1)
+
+            if [[ $START_PARTITION_MONTH -eq 0 ]]; then
+                START_PARTITION_MONTH=12
+                START_PARTITION_YEAR=$(expr $START_PARTITION_YEAR - 1)
+            fi
+            if [[ ${START_PARTITION_MONTH} =~ ^[0-9]$ ]]; then
+                START_PARTITION_MONTH="0${START_PARTITION_MONTH}"
+            fi
+        done
+    fi
+
+    # プライマリキーの設定
+    set_primarykey_sql
+
+    # SQL文作成処理(テーブル毎に実行)
+    for table_name in "history" "history_uint" "history_str" "history_log" "history_text" "trends" "trends_uint"
+    do
+        make_partition_sql_by_table $table_name $START_PARTITION_YEAR $START_PARTITION_MONTH $GLOBAL_PARTITION_COUNT
+    done
